3c5c5234f9f3d32efdc7517c368d5dc9d44bc4dc
SpaceMC Network <126994541+SpaceMCNetwork@users.noreply.github.com>
2026-07-01 05:52:15 -0500

    Update DMZPlaceholders.java

--- src/main/java/me/daniramos/dmzplaceholders/DMZPlaceholders.java
+++ src/main/java/me/daniramos/dmzplaceholders/DMZPlaceholders.java
@@ -1,1 +1,36 @@
+package me.daniramos.dmzplaceholders;
 
+import me.clip.placeholderapi.PlaceholderAPIPlugin;
+import me.daniramos.dmzplaceholders.expansion.DMZExpansion;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DMZPlaceholders extends JavaPlugin {
+
+    private static DMZPlaceholders instance;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            getLogger().severe("PlaceholderAPI no encontrado.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        new DMZExpansion(this).register();
+
+        getLogger().info("DMZPlaceholders habilitado correctamente.");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public static DMZPlaceholders getInstance() {
+        return instance;
+    }
+
+}
